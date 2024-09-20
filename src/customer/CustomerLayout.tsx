f366b0914e8ebdd1b3f249f667de856fd736fce3
Saroj Aryal <sarojaryal2003@gmail.com>
2024-09-20 15:59:27 +0545

    add screenshots

--- src/customer/CustomerLayout.tsx
+++ src/customer/CustomerLayout.tsx
@@ -1,14 +1,11 @@
-import React from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/bar/Navbar";
-import OfferBar from "../components/bar/OfferBar";
 import Footer from "../components/Footer";
 
 const Layout = () => {
   return (
     <>
       <header className="shadow-sm sticky top-0 left-0 z-10 bg-white">
-        <OfferBar />
         <Navbar />
       </header>
       <main className="container">
