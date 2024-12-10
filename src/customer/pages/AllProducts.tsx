4f8da953810489949b1f5ff65f75651447996486
saroj <dev.saroj018@gmail.com>
2024-12-10 19:16:57 +0545

    feat:  loading state UI in AllProducts component

--- src/customer/pages/AllProducts.tsx
+++ src/customer/pages/AllProducts.tsx
@@ -84,9 +84,22 @@ const AllProducts = () => {
         </div>
       </div>
       {loading && data && (
-        <ParaTypo className="text-4xl text-red-500 font-semibold text-center my-4">
-          Loading...........
-        </ParaTypo>
+        <div className="flex items-center justify-center space-x-1 text-sm text-gray-700">
+          <svg
+            fill="none"
+            className="h-6 w-6 animate-spin"
+            viewBox="0 0 32 32"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              clipRule="evenodd"
+              d="M15.165 8.53a.5.5 0 01-.404.58A7 7 0 1023 16a.5.5 0 011 0 8 8 0 11-9.416-7.874.5.5 0 01.58.404z"
+              fill="currentColor"
+              fillRule="evenodd"
+            />
+          </svg>
+          <div className="text-xl">Loading ...</div>
+        </div>
       )}
     </div>
   );
